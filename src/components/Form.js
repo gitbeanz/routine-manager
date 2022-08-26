23c9643db0ba019af98d2837a7636536942eaf16
gitbeanz <74123124+gitbeanz@users.noreply.github.com>
2022-08-26 02:53:11 -0700

    Implemented form component and tweaked it

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,8 +6,8 @@ export default function Form(props) {
 
   return (
     <div className={props.formClass}>
-        <form>
-            <input type="text" placeholder={props.placeholder} onChange={event => props.changeFunct(event)}></input>
+        <form onKeyDown= {function(event){return event.key !== 'Enter'}} onSubmit={event=>{event.preventDefault()}}>
+            <input type="text" placeholder={props.placeholder} onChange={event => props.changeFunct(event) }></input>
             <button onClick={props.clickFunct} type="button">{props.btnPlaceholder}</button>
         </form>
     </div>
