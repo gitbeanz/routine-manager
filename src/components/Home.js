89cf25ff063a7edd213250fe5aa2baf61281fe00
gitbeanz <74123124+gitbeanz@users.noreply.github.com>
2022-08-26 02:55:10 -0700

    Implemented addition of routines and modified handling button

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,33 @@
 import React from 'react'
 import useRoutineManager from '../hooks/useRoutineManager';
 import Form from './Form'
+import Routine from './Routine';
+var isEmpty = true;
 
 export default function Home() {
     const { count, handleCountClick, handleRoutineName, routineArray} = useRoutineManager();
     function handleClick(){
+      if (!isEmpty){
         handleCountClick();
-        console.log(routineArray);
+      }
+        //console.log(routineArray);
     }
     function handleChange(event){
+      if (event.target.value === ''){
+        isEmpty = true;
+      }
+      else{
+        isEmpty = false;
+      }
         handleRoutineName(event.target.value);
     }
-    var renderedTasks = routineArray.map(item=> <div>{item}</div>);
+    /*function handleSubmit(key){
+      if (key.key === 'Enter'){
+        handleClick();
+      }
+    }
+    window.addEventListener('keyup',handleSubmit);*/
+    var renderedRoutines = routineArray.map(item=> <Routine title={item} subtitle='NEW' routineClass='new' key={item}/>);
   return (
     <div>
         <h1 className="home-h1">Routine Manager</h1>
@@ -19,8 +35,11 @@ export default function Home() {
         <h2 className="home-h2">Add Routines</h2>
         <Form changeFunct={handleChange} placeholder="Enter new routine name..." formClass="home-form" btnPlaceholder="+" clickFunct={handleClick}/>
         <h2 className='home-h2'>My Routines ({count})</h2>
-        {renderedTasks}
-
+        <div className="home-routine-section">
+        {renderedRoutines}
+        </div>
+        <hr className="divider"/>
+        <footer>© 2022 Designed with &lt;3 By Brandon Gumayagay</footer>
     </div>
   )
 }
