ead7849eb67067a99f645a66d7e1a932ad363ac9
gitbeanz <74123124+gitbeanz@users.noreply.github.com>
2022-09-05 16:58:40 -0700

    Added Trash component that acts as a model for when the user tries to delete a routine"

---
+++ src/components/Trash.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Trash(props) {
+  return (
+    <div className='add-overlay'>
+        <div className='trash-box'>
+            <p className='delete-msg'>Are you sure you want to delete this routine?</p>
+            <button className='trash-cancel-button' onClick={props.cancelFunct}>Cancel</button>
+            <button className='trash-yes-button' onClick={props.deleteFunct}>Yes</button>
+        </div>
+    </div>
+  )
+}
