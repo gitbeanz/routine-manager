0149352413175213e1b449fb60f9033725f2bf0d
gitbeanz <74123124+gitbeanz@users.noreply.github.com>
2022-09-05 01:27:51 -0700

    Updated routine manager to include routine data functionality

--- src/hooks/useRoutineManager.js
+++ src/hooks/useRoutineManager.js
@@ -7,6 +7,9 @@ const useRoutineManager = () => {
     const [modal, showModal] = useState(false);
     const [errorMessage, setMessage] = useState('');
     const [homeStatus, setHomeState] = useState(true);
+    const [ routineData, setRoutineData] = useState({});
+    const [dataArray, setDataArray] = useState([]);
+    const [addModal, setAddModal] = useState(false);
 
     const handleCountClick = () => {
         //increments routine count and appends to routineArray
@@ -16,18 +19,18 @@ const useRoutineManager = () => {
         setRoutineArray([...routineArray, currentRoutine]);
         }
         else if (routineArray.includes(currentRoutine)){
-            setMessage('Woops! You already have a routine with this name. Click anywhere to try again')
+            setMessage('Whoops! You already have a routine with this name. Click anywhere to try again')
             showModal(true);
         }
         else if (currentRoutine.length === 0){
-            setMessage("Woops! Don't leave your routine name blank. Click anywhere to try again")
+            setMessage("Whoops! Don't leave your routine name blank. Click anywhere to try again")
             showModal(true);
         }
         setCurrentRoutine('');
     }
 
     const handleRoutineName = (routine) => {
-        console.log('this now passed', routine);
+        //console.log('this now passed', routine);
         setCurrentRoutine(routine);
         return currentRoutine;
     }
@@ -42,8 +45,13 @@ const useRoutineManager = () => {
         console.log(homeStatus);
         return homeStatus;
     }
+    const makeRoutineData = (title, time) => {
+        //make new routine data, add to routine array
+        setRoutineData({title: title, time: time, taskArray: []});
+        setDataArray([...dataArray, routineData]);
+    }
 
-    return { count, handleCountClick, currentRoutine, handleRoutineName, routineArray, modal, showModal, errorMessage, homeStatus, routinePageOpen, homePageOpen};
+    return { count, handleCountClick, currentRoutine, handleRoutineName, routineArray, modal, showModal, errorMessage, homeStatus, routinePageOpen, homePageOpen, makeRoutineData, routineData, addModal, setAddModal};
 }
 
 export default useRoutineManager;
