39405f2bd759cf3ed902e071fca76ce0b0e67f19
gitbeanz <74123124+gitbeanz@users.noreply.github.com>
2022-09-06 19:48:35 -0700

    Tweaked input for minutes field to ensure users only enter valid input (integers with 2 digits)

--- src/components/Add.js
+++ src/components/Add.js
@@ -2,15 +2,18 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAdd } from '@fortawesome/free-solid-svg-icons'
 export default function Add(props) {
+  function handleAddTask(){
+    ;
+  }
   return (
     <div className='add-overlay'>
         <div className='add-box'>
-            <form className='add-form' onSubmit={event=>{event.preventDefault()}}>
-                <label className='name'>Name</label>
-                <input className='name-input' placeholder='Enter task name...'></input>
-                <label className='time'>Time</label>
-                <input className='time-input' placeholder='Enter time... (min)'type="number"></input>
-                <button className='add-button'><FontAwesomeIcon icon={faAdd}/></button>
+            <form onKeyDown= {function(event){return event.key !== 'Enter'}} className='add-form' onSubmit={event=>{event.preventDefault()}}>
+                <label className='name' required>Name</label>
+                <input className='name-input' placeholder='Enter task name...' onChange={event => props.changeFunct(event) }></input>
+                <label className='time' required>Minutes</label>
+                <input type='number' min='1' step='1' className='time-input' placeholder='Enter time... (min)' onChange={event => props.changeFunctTime(event) }></input>
+                <button className='add-button' onClick={handleAddTask}><FontAwesomeIcon icon={faAdd}/></button>
                 <button className='cancel-button' onClick={props.cancelFunct}>Cancel</button>
             </form>
         </div>
--- src/hooks/useRoutineManager.js
+++ src/hooks/useRoutineManager.js
@@ -1,3 +1,4 @@
+
 import { useState } from "react";
 
 const useRoutineManager = () => {
@@ -10,6 +11,8 @@ const useRoutineManager = () => {
     const [ routineData, setRoutineData] = useState({});
     const [dataArray, setDataArray] = useState([]);
     const [addModal, setAddModal] = useState(false);
+    const [trashModal, setTrashModal] = useState(false);
+    const [currentTime, setTime] = useState(0);
 
     const handleCountClick = () => {
         //increments routine count and appends to routineArray
@@ -50,8 +53,42 @@ const useRoutineManager = () => {
         setRoutineData({title: title, time: time, taskArray: []});
         setDataArray([...dataArray, routineData]);
     }
+    const routineDelete = (id) => {
+        setDataArray(dataArray.filter(element => {
+            if (element.title !== id){
+                console.log('found');
+                return true;
+            }
+            else{
+                return false;
+            }
+        }));
+        setRoutineArray(routineArray.filter(element=>{
+            if (element !== id){
+                console.log('found');
+                return true;
+            }
+            else{
+                return false;
+            }
+        }))
+        setCount(count -1);
+        setHomeState(true);
+    }
+    const timeChange = (number)=>{
+        console.log(number);
+        console.log(typeof(number));
+        if (number === ''){
+            ;
+        }
+        else{
+            setTime(parseInt(number));
+            console.log(currentTime);
+            } 
+        }
 
-    return { count, handleCountClick, currentRoutine, handleRoutineName, routineArray, modal, showModal, errorMessage, homeStatus, routinePageOpen, homePageOpen, makeRoutineData, routineData, addModal, setAddModal};
+
+    return { count, handleCountClick, currentRoutine, handleRoutineName, routineArray, modal, showModal, errorMessage, homeStatus, routinePageOpen, homePageOpen, makeRoutineData, routineData, addModal, setAddModal, trashModal, setTrashModal, routineDelete, timeChange, currentTime};
 }
 
 export default useRoutineManager;
