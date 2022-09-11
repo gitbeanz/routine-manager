5235fe4f65ae5365bed58866bd41018d93eaa0a6
gitbeanz <74123124+gitbeanz@users.noreply.github.com>
2022-09-11 14:30:10 -0700

    Modified trash message so that it can work for both routines and tasks

--- src/components/Trash.js
+++ src/components/Trash.js
@@ -4,7 +4,7 @@ export default function Trash(props) {
   return (
     <div className='add-overlay'>
         <div className='trash-box'>
-            <p className='delete-msg'>Are you sure you want to delete this routine?</p>
+            <p className='delete-msg'>Are you sure you want to delete this {props.type}?</p>
             <button className='trash-cancel-button' onClick={props.cancelFunct}>Cancel</button>
             <button className='trash-yes-button' onClick={props.deleteFunct}>Yes</button>
         </div>
