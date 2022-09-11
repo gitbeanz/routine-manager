6d6aa71b8d051a6fb5b42e7ae2875e8fda9b738d
gitbeanz <74123124+gitbeanz@users.noreply.github.com>
2022-09-11 14:29:47 -0700

    Added click functionality so that when clicked, tasks can be deleted

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Task(props) {
     return (
-        <div id={props.title} className='routine'>
+        <div onClick={props.clickFunct}id={props.title} className='routine'>
             <p className='time-subtitle'>{props.subtitle} min</p>
             <p className='title'>{props.title}</p>
             </div>
