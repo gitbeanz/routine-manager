4427aaaafdc92766d3efb6834988d97ee951f06a
gitbeanz <74123124+gitbeanz@users.noreply.github.com>
2022-09-11 02:41:49 -0700

    Edited the Routine functions props so that it works with the data array and not the string array

--- src/components/Routine.js
+++ src/components/Routine.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
 
+
 export default function Routine(props) {
   return (
     <div id={props.title} className='routine' onClick={props.routineClick}>
-        <p className={props.routineClass}>{props.subtitle}</p>
+        <p className={props.routineClass}>{props.subtitle} min</p>
         <p className='title'>{props.title}</p>
         </div>
   )
