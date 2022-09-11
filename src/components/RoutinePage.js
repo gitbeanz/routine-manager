6173c472091ff399fd51b91b331a0732faf536bb
gitbeanz <74123124+gitbeanz@users.noreply.github.com>
2022-09-11 14:29:17 -0700

    Added functionality for saving updated routine data and added functionality for deleting tasks and checking for task input validity

--- src/components/RoutinePage.js
+++ src/components/RoutinePage.js
@@ -9,7 +9,7 @@ import Error from './Error'
 import Trash from './Trash'
 import Task from './Task'
 export default function RoutinePage(props) {
-  const {taskCount, taskArray, taskChange, timeChange, trashModal, setTrashModal, addModal, setAddModal, modal, showModal, addTask } = useRoutineManager();
+  const {taskSelected, setSelectedTask, showTaskDeleteModal, taskDeleteModal, checkTaskValidity, taskModal, showTaskModal, taskError, taskCount, taskArray, taskChange, timeChange, trashModal, setTrashModal, addModal, setAddModal, modal, showModal, addTask } = useRoutineManager();
   var errorMessage = "Whoops! You can't start a routine without adding a task. Click anywhere to try again"
   function handleAdd(){
     setAddModal(true);
@@ -23,6 +23,10 @@ export default function RoutinePage(props) {
   function handleErrorClick(){
     showModal(false);
   }
+  function handleTaskErrorClick(){
+    showTaskModal(false);
+    setAddModal(true);
+  }
   function handleStart(){
     showModal(true);
   }
@@ -32,35 +36,59 @@ export default function RoutinePage(props) {
   function confirmDelete(){
     props.deleteFunct(props.title);
   }
+  function confirmTaskDelete(){
+    console.log('this ran');
+    props.deleteTaskFunct(taskSelected);
+    showTaskDeleteModal(false);
+  }
+  function cancelTaskDelete(){
+    showTaskDeleteModal(false);
+  }
+  function deleteTask(event){
+    setSelectedTask(event.target.id);
+    showTaskDeleteModal(true);
+  }
   function handleTimeChange(event){
     if (event.target.value.length > 2){
       //console.log('too long');
       event.target.value = event.target.value.slice(0,2);
     }
     ;
-    timeChange(event.target.value);
+    props.timeChange(event.target.value);
   }
   function handleTaskChange(event){
-    taskChange(event.target.value);
+    props.taskChange(event.target.value);
+  }
+  function resetInput(){
+    props.timeChange(0);
+    props.taskChange('');
   }
   function handleAddTask(){
-    let taskData = addTask();
+    if (checkTaskValidity(props.task,props.taskTime,props.taskArray) === true){
+    let taskData = props.addTask();
     console.log(taskArray);
     props.timeSet(props.totalTime+parseInt(taskData.time));
     setAddModal(false);
+    resetInput();
+  }
+  else{
+    resetInput();
+  }
   }
   //console.log(typeof(taskArray));
-  var renderedTasks = taskArray.map(item => <Task title={item.title} subtitle={item.time} routineClass='new' key={item.title}/>);
+  var renderedTasks = props.taskArray.map(item => <Task clickFunct={deleteTask}title={item.title} subtitle={item.time} routineClass='new' key={item.title}/>);
   return (
     <div>
+      {taskModal && <Error clickFunct={handleTaskErrorClick} errorText={taskError}/>}
       {addModal && <Add addFunct={handleAddTask}changeFunct={handleTaskChange} changeFunctTime={handleTimeChange} cancelFunct={cancelTask}/>}
       {modal && <Error  clickFunct={handleErrorClick}errorText={errorMessage} />}
-      {trashModal && <Trash deleteFunct={confirmDelete}cancelFunct={cancelDelete}/>}
+      {trashModal && <Trash type='routine'deleteFunct={confirmDelete}cancelFunct={cancelDelete}/>}
+      {taskDeleteModal && <Trash type ='task' deleteFunct={confirmTaskDelete} cancelFunct={cancelTaskDelete}/>}
     <div className='routine-div'>
         <h1 className='routine-title'>{props.title}</h1>
         <button className='routine-home-button'onClick={props.clickFunct}><FontAwesomeIcon icon={faHome}></FontAwesomeIcon></button>
         <h2 className='routine-subtitle'>{props.totalTime} minutes</h2>
-        <h2 className='routine-task-title'>My Tasks ({taskCount})</h2>
+        <h2 className='routine-task-title'>My Tasks ({props.taskCount})</h2>
     </div>
     <div className='routine-task-section'>
       {renderedTasks}
