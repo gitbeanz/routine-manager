5ff1889136ff0206fc1e24ed032bf90cc21c9530
gitbeanz <74123124+gitbeanz@users.noreply.github.com>
2022-09-23 15:53:23 -0700

    Added a subtitle for showing the difference between task time and actual time

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function Summary(props) {
   return (
+    <div>
     <h3 className='summary-task-h3'>{props.title}</h3>
+    <h3 color={props.color}>{props.difference}</h3>
+    </div>
   )
 }
