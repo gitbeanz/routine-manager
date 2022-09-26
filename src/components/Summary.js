e7ae009aa153337197969b047bf22acc49e1d253
gitbeanz <74123124+gitbeanz@users.noreply.github.com>
2022-09-26 14:19:14 -0700

    Added difference and original time to summary component

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -4,7 +4,10 @@ export default function Summary(props) {
   return (
     <div>
     <h3 className='summary-task-h3'>{props.title}</h3>
-    <h3 color={props.color}>{props.difference}</h3>
+    <div className='summary-description-div'>
+    <h3 className='summary-time-h3'>{props.time} min</h3>
+    <h3 className='summary-difference-h3'style={{color: props.color}}>{props.difference}</h3>
+    </div>
     </div>
   )
 }
